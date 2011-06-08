c216eae2c997b2a4c4be352d22a518c2824ae23d
Jens Låås <jens@laas.mine.nu>
2011-06-08 13:31:48 +0200

    -l --list option

--- devinfo.c
+++ devinfo.c
@@ -13,10 +13,37 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 #include "libdevname.h"
+#include "devlookup.h"
 
 #include "jelopt.h"
+
+static int strsuffix(const char *s, const char *suf)
+{
+	if(strlen(suf) > strlen(s))
+		return 0;
+	s+=(strlen(s)-strlen(suf));
+	return strcmp(s, suf)==0;
+}
+
+static void conf_lookup(const char *pfx, DIR *dir)
+{
+	struct dirent *ent;
+	char *p, name[256], node[256];
+	while( (ent=readdir(dir)) ) {
+		if(strsuffix(ent->d_name, ".conf")) {
+			strncpy(name, ent->d_name, sizeof(name));
+			p = strrchr(name, '.');
+			if(p) *p = 0;
+			if(devname_lookup(node, sizeof(node), name)>0)
+				printf("%s: %s -> %s\n", pfx, name, node);
+			else
+				printf("%s: %s not found\n", pfx, name);
+		}
+	}	
+}
 
 int main(int argc, char **argv)
 {
@@ -33,8 +60,32 @@ int main(int argc, char **argv)
 	if(jelopt(argv, 'h', "help", NULL, &err)) {
 		printf("devname-info [-h]\n"
 		       " version " LIBDEVNAME_VERSION "\n"
+		       " -l --list -- list devnames and lookup info\n"
 		       " Scans for and prints device information suitable as arguments for the\n"
 		       " 'devname' program.\n");
+		exit(0);
+	}
+
+	if(jelopt(argv, 'l', "list", NULL, &err)) {
+		DIR *dir;
+		char *home, fn[256];
+		
+		strcpy(fn, "/etc/devname.d");
+		dir = opendir(fn);
+		if(dir) {
+			conf_lookup("sys", dir);
+			closedir(dir);
+		}
+		home = getenv("HOME");
+		if(home) {
+			snprintf(fn, sizeof(fn), "%s/.devname.d", home);
+			dir = opendir(fn);
+			if(dir) {
+				conf_lookup("user", dir);
+				closedir(dir);
+			}
+		}
+
 		exit(0);
 	}
 
